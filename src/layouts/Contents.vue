81aeb6f2491665d6408c3866f389a543a437a7af
soljjang777 <vivace8520vi@naver.com>
2023-12-02 14:37:43 +0900

    contents 파일

---
+++ src/layouts/Contents.vue
@@ -0,0 +1,15 @@
+<template>
+  <div style="border: 2px black solid;">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contents"
+}
+</script>
+
+<style scoped>
+
+</style>
